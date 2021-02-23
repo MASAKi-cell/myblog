4021687ca224ae1aa37b64409aae280b342e348c
MASAKi-cell <74603325+MASAKi-cell@users.noreply.github.com>
2021-02-23 09:46:57 +0900

    footer.phpの追加

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap core JavaScript -->
+<script src="http//my-site.local/wp-content/themes/my_blog/vendor/jquery/jquery.min.js"></script>
+<script src="http//my-site.local/wp-content/themes/my_blog/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Custom scripts for this template -->
+<script src="js/clean-blog.min.js"></script>
+
+<?php wp_footer(); ?>
