e1f331c4af8d3bc242124eca9818ad172fd5f6ca
MASAKi-cell <74603325+MASAKi-cell@users.noreply.github.com>
2021-03-08 21:48:37 +0900

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,25 +1,26 @@
   <!-- ここからNavigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
       </button>
+      <?php
+        //メニューIDを取得 決まり文句
+        $menu_name = 'global_nav';
+        $locations = get_nav_menu_locations();
+        $menu = wp_get_nav_menu_object($locations[$menu_name]);
+        $menu_items = wp_get_nav_menu_items($menu -> term_id);
+      ?>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="index.php">Home</a>
+        <!-- メニューの繰り返し構文 $menu_itemsの内容をforeachで1つずつ取り出しながら -->
+        <?php foreach($menu_items as $item): ?> 
+          <li class="nav-item"> <!--esc = Escape(エスケープ処理)esc_attr urlはhref属性として出力する為、esc_html メニューの部分はhtmlで出力する為-->
+            <a class="nav-link" href="<?php echo esc_attr($item->url); ?>"><?php echo esc_html($item->title); ?></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.php">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="post.php">Sample Post</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.php">Contact</a>
-          </li>
+          <?php endforeach; ?>
         </ul>
       </div>
     </div>
